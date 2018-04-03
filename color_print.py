a09262e6dd4bbbb1f91edf97df8ef270002d7139
Karakurt <karakurt@mail.ru>
2018-04-04 00:19:28 +0500

    change

---
+++ color_print.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#https://pypi.python.org/pypi/colorama
+
+#from termcolor import colored
+#print(colored("Это будет выводиться красным", 'red'))
+print('\033[96mТекст будет голубым\033[0m')
