b76301a40965804998ac6463c99a1b48c50596d5
Evgeniy Rozhkov <karakurt@mail.ru>
2018-04-04 17:34:24 +0500

    change
    Лять, какой же питон привередливый к оформлению!

--- guess_any_number_mac.py
+++ guess_any_number_mac.py
@@ -5,17 +5,19 @@
 
 # the_number = random.randint(1, 100)
 
+min_num = 1
+max_num = 100
+
 print('Загадайте число от 1 до 100, а я попробую его угадать.')
 num = 0
 count = 0
-while (num < 1 or num > 100 or count == 0):
+while (num < min_num or num > max_num or count == 0):
     if count > 0:
-      print('Не верный диапазон, число болжно быть в пределах от 1 до 100 включительно!')
+        print('Не верный диапазон, число болжно быть в пределах от ',
+              min_num, ' до ', max_num, ' включительно!')
     count += 1
     num = int(input('Введите число: '))
 
-min_num = 1
-max_num = 100
 count = 0
 while True:
     count += 1
