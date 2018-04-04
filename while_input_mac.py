f043cfe6b5a8f18a9ea13f0d7d1ee9f5ea53f79f
Evgeniy Rozhkov <karakurt@mail.ru>
2018-04-04 16:55:08 +0500

    change

--- while_input_mac.py
+++ while_input_mac.py
@@ -1,17 +1,32 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-#import random
+# import random
 
-#the_number = random.randint(1, 100)
+# the_number = random.randint(1, 100)
 
-print('Загадайте число от 1 до 100')
+print('Загадайте число от 1 до 100, а я попробую его угадать.')
 num = 0
 count = 0
 while (num < 1 or num > 100 or count == 0):
     if count > 0:
-        print('Не верный диапазон, число болжно быть в пределах от 1 до 100 включительно!')
+      print('Не верный диапазон, число болжно быть в пределах от 1 до 100 включительно!')
     count += 1
     num = int(input('Введите число: '))
 
-print('Вы ввели: ', num, ' с ', count, 'попыток')
+min_num = 1
+max_num = 100
+count = 0
+while True:
+    count += 1
+    avr_num = int(min_num + (max_num - min_num) / 2)
+    print ('Среднее число между ', min_num, ' и ', max_num, ' = ', avr_num)
+    if avr_num == num:
+        print('Вы загадали число: ', avr_num)
+        print('Я отгадал с попытки: ', count)
+        break
+    elif avr_num < num:
+        min_num = avr_num
+    elif avr_num > num:
+        max_num = avr_num
+# print('Вы ввели: ', num, ' с ', count, 'попыток')
