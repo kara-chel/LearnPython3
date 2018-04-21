4bb465267ead783e0e0ca3b2610f08f4036ecb80
Karakurt <karakurt@mail.ru>
2018-04-22 00:33:46 +0500

    chande chadge rename change

--- mosquito.py
+++ mosquito.py
@@ -1,2 +1,16 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+import sys
+import pygame
+
+# WIN_WIDTH = 800 #Ширина создаваемого окна
+# WIN_HEIGHT = 640 # Высота
+# size = width, height = 320, 240
+
+
+def game_main():
+	pygame.init()
+	sys.exit()
+
+game_main()
